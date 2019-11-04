c52b34c21cdd05038821b0d34522c7c785adb273
ZhangJunjie <zjj19950716@gmail.com>
2019-11-04 16:36:49 +0800

    Http文件上传

--- simpleHttp.go
+++ simpleHttp.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"crypto/tls"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -99,4 +103,57 @@ func Post(c context.Context, uri string, params url.Values) (body []byte, err er
 	}
 	body, err = Do(c, req)
 	return
+}
+
+func NewUploadRequest(link string, params map[string]string, name, path string) (*http.Request, error) {
+	//打开文件句柄操作
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("error opening file")
+		return nil, err
+	}
+	defer file.Close()
+
+	//创建一个模拟的form中的一个选项,这个form项现在是空的
+	bodyBuf := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(bodyBuf)
+
+	//关键的一步操作, 设置文件的上传参数叫uploadfile, 文件名是filename,
+	//相当于现在还没选择文件, form项里选择文件的选项
+	fileWriter, err := bodyWriter.CreateFormFile("file", path)
+	if err != nil {
+		fmt.Println("error writing to buffer")
+		return nil, err
+	}
+
+	//iocopy 这里相当于选择了文件,将文件放到form中
+	_, err = io.Copy(fileWriter, file)
+	if err != nil {
+		return nil, err
+	}
+
+	//获取上传文件的类型,multipart/form-data; boundary=...
+	contentType := bodyWriter.FormDataContentType()
+
+	//这里就是上传的其他参数设置,可以使用 bodyWriter.WriteField(key, val) 方法
+	//也可以自己在重新使用  multipart.NewWriter 重新建立一项,这个再server 会有例子
+	//这种设置值得仿佛 和下面再从新创建一个的一样
+	for key, val := range params {
+		e := bodyWriter.WriteField(key, val)
+		if e != nil {
+			return nil, e
+		}
+	}
+
+	//这个很关键,必须这样写关闭,不能使用defer关闭,不然会导致错误
+	bodyWriter.Close()
+
+	//发送post请求到服务端
+	request, err := http.NewRequest(http.MethodPost, link, bodyBuf)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("Content-Type", contentType)
+	return request, err
+
 }
